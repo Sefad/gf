01e43768e7aa7e91798b6010ffd963b0f32b15cc
Vlad <sefad2008@yandex.ru>
2016-12-06 14:01:46 +0300

    nonfarm: created separate phone version

---
+++ nonfarm/phone/js/default.js
@@ -0,0 +1,43 @@
+$( document ).ready(function() {
+	var first = $(".firstInfo"),
+		second = $(".secondInfo"),
+		third = $(".thirdInfo");
+	if (window.matchMedia("(min-width: 1200px)").matches) {
+	    first.hover(function() {
+		    first.addClass("hover");
+		}, function() {
+		    first.removeClass("hover");
+		});
+		second.hover(function() {
+		    second.addClass("hover");
+		}, function() {
+		    second.removeClass("hover");
+		});
+		third.hover(function() {
+		    third.addClass("hover");
+		}, function() {
+		    third.removeClass("hover");
+		});
+	};
+	if (window.matchMedia("(max-width: 1199px)").matches) {
+		first.on('click touchstart', function(e) {
+			if(first.hasClass("hover")){
+			} else {
+				first.addClass("hover");
+			}
+		});
+		second.on('click touchstart', function(e) {
+			if(second.hasClass("hover")){
+			} else {
+				second.addClass("hover");
+			}
+		});
+		third.on('click touchstart', function(e) {
+			if(third.hasClass("hover")){
+			} else {
+				third.addClass("hover");
+			}
+		});
+	};
+	$(".lgroup_datepicker").datepicker($.datepicker.regional["ru"]); $(".lgroup_datepicker").datepicker("option", "yearRange", "-100:+20"); $(".lgroup_datepicker").blur();
+});
